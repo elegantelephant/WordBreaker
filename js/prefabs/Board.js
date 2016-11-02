acd00e543ee7c80ee8d2c662bb25b6573160cebe
Rob Brown <bbb@cpan.org>
2016-11-02 11:15:49 -0600

    tabs unblooding!

--- js/prefabs/Board.js
+++ js/prefabs/Board.js
@@ -6,8 +6,8 @@ var Board = {};
 Board.createBoard = function(level) {
     this.SIZEX = WB.game.world.width;
     this.SIZEY = WB.game.world.height;
-	var rows = 9;
-	var columns = 5;
+    var rows = 9;
+    var columns = 5;
 
     Board.generateGrid(rows, columns);
     Board.generateWordText();
@@ -16,13 +16,13 @@ Board.createBoard = function(level) {
 
 Board.generateGrid = function(rows, cols) {
     this.board = [];
-	var index = 0;
+    var index = 0;
     var pixel_x;
     var pixel_y;
     this.tileSize = Math.floor(this.SIZEX / 8);
     this.texts = WB.game.add.group();
     for (var x = 0; x < cols; x++) {
-        this.board[x] = []; 
+        this.board[x] = [];
         for (var y = 0; y < rows; y++) {
             pixel_x = (x + 1.5) * this.tileSize;
             pixel_y = (this.SIZEY) - (y + 1.5) * this.tileSize;
@@ -30,14 +30,14 @@ Board.generateGrid = function(rows, cols) {
             Board.addTile(pixel_x, pixel_y, x, y);
             Board.addLetter(pixel_x, pixel_y, x, y);
         }
-    }   
+    }
 };
 
 Board.addTile = function(pixX, pixY, x, y) {
     tile = WB.game.add.button(pixX, pixY, 'letter_tile', Board.clicked, this);
     tile.scale.setTo(this.tileSize / tile.width);
     tile.anchor.setTo(0.5);
-    tile.customParams = {}; 
+    tile.customParams = {};
     tile.customParams.x = x;
     tile.customParams.y = y;
 };
@@ -48,7 +48,7 @@ Board.addLetter = function(pixX, pixY, x, y) {
         text = WB.game.add.text(pixX, pixY);
         text.anchor.setTo(0.5);
     }
-    text.customParams = {}; 
+    text.customParams = {};yyyyyyyyyyyyyyyyyyyy
     text.customParams.x = x;
     text.customParams.y = y;
 
