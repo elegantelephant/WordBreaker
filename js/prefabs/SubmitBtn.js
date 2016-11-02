a3a23d9e033c51ad327530a539a4c12746d0ed61
Rob Brown <bbb@cpan.org>
2016-11-02 14:44:22 -0600

    tabs unblooded harder

--- js/prefabs/SubmitBtn.js
+++ js/prefabs/SubmitBtn.js
@@ -6,9 +6,9 @@ var SubmitBtn = {};
 SubmitBtn.createSubmitBtn = function(callback) {
     this.GAME_WIDTH = WB.game.world.width;
     this.GAME_HEIGHT = WB.game.world.height;
-    
+
     var btn = WB.game.add.button(this.GAME_WIDTH / 2 , this.GAME_HEIGHT / 5, 'submit_button', callback, this);
-   
+
     btn.anchor.setTo(0, 0); // Set anchor to the bottom/center
     btn.scale.setTo((this.GAME_WIDTH/btn.width)*0.5);
 };
