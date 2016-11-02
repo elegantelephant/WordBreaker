597a9eee595bdb43854f7ebc8bee72d69fcb21aa
DizzyDogg <jeffthetenor@yahoo.com>
2016-11-02 16:00:29 -0600

    restructured letters and tiles into one object

--- js/prefabs/Board.js
+++ js/prefabs/Board.js
@@ -10,11 +10,12 @@ Board.create = function(level) {
     this.columns = 5;
 
     // create and initialize the tiles structure
-    this.tiles = Array(this.columns);
+    this.board = Array(this.columns);
     for (var col = 0; col < this.columns; col ++) {
-        this.tiles[col] = Array(this.rows);
+        this.board[col] = Array(this.rows);
         for (var row = 0; row < this.rows; row ++) {
-            this.tiles[col][row] = { selected: 0, };
+            this.board[col][row] = {};
+            this.board[col][row].selected = 0;
         }
     }
 
@@ -26,14 +27,12 @@ Board.create = function(level) {
 };
 
 Board.generateGrid = function(rows, cols) {
-    this.board = [];
     var index = 0;
     var pixel_x;
     var pixel_y;
     this.tileSize = Math.floor(this.SIZEX / 8);
     this.texts = WB.game.add.group();
     for (var x = 0; x < cols; x++) {
-        this.board[x] = [];
         for (var y = 0; y < rows; y++) {
             pixel_x = (x + 1.5) * this.tileSize;
             pixel_y = (this.SIZEY) - (y + 1.5) * this.tileSize;
@@ -48,10 +47,10 @@ Board.addTile = function(pixX, pixY, x, y) {
     tile = WB.game.add.button(pixX, pixY, 'letter_tile', Board.clicked, this);
     tile.scale.setTo(this.tileSize / tile.width);
     tile.anchor.setTo(0.5);
-    tile.customParams = {};
-    tile.customParams.x = x;
-    tile.customParams.y = y;
-    this.tiles[x][y].tile = tile;
+    tile.gridx = x;
+    tile.gridy = y;
+    this.board[x][y].tile = tile;
+    
 };
 
 Board.addLetter = function(pixX, pixY, x, y) {
@@ -60,22 +59,19 @@ Board.addLetter = function(pixX, pixY, x, y) {
         text = WB.game.add.text(pixX, pixY);
         text.anchor.setTo(0.5);
     }
-    text.customParams = {};
-    text.customParams.x = x;
-    text.customParams.y = y;
 
     // Choose random letter from pool
     text.text = this.letterPool.charAt(Math.floor(Math.random() * this.letterPool.length)).toUpperCase();
     text.style.font = 'bold 22pt Arial';
     text.style.fill = '#22f';
     this.texts.add(text);
-    this.board[x][y] = text;
+    this.board[x][y].text = text;
 };
 
 Board.clicked = function(button) {
-    var x = button.customParams.x;
-    var y = button.customParams.y;
-    if (! this.tiles[x][y].selected
+    var x = button.gridx;
+    var y = button.gridy;
+    if (! this.board[x][y].selected
        && (typeof this.prevx === 'undefined'
          || ((Math.abs(x - this.prevx) < 2
            && Math.abs(y - this.prevy) < 2))
@@ -84,16 +80,16 @@ Board.clicked = function(button) {
         button.alpha = 0.7;
         this.prevx = x;
         this.prevy = y;
-        this.tiles[x][y].selected = 1;
-        this.currentWord.text += this.board[button.customParams.x][button.customParams.y].text;
+        this.board[x][y].selected = 1;
+        this.currentWord.text += this.board[button.gridx][button.gridy].text.text;
     }
 };
 
 Board.deselectAll = function() {
     for (var col=0; col < this.columns; col++) {
         for (var row=0; row < this.rows; row++) {
-            this.tiles[col][row].tile.alpha = 1.0;
-            this.tiles[col][row].selected = 0;
+            this.board[col][row].tile.alpha = 1.0;
+            this.board[col][row].selected = 0;
         }
     }
     delete this.prevx;
